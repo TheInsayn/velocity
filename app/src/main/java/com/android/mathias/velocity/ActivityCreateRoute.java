e150fbdf48b61963da8bc6bdb225c083d7dcc55b
Mathias Wöß <mat.woess@gmail.com>
2017-01-19 22:50:38 +0100

    handle map drag correctly

--- app/src/main/java/com/android/mathias/velocity/ActivityCreateRoute.java
+++ app/src/main/java/com/android/mathias/velocity/ActivityCreateRoute.java
@@ -136,14 +136,21 @@ public class ActivityCreateRoute extends AppCompatActivity implements
             public void onMarkerDrag(Marker marker) { }
             @Override
             public void onMarkerDragEnd(Marker marker) {
+                String address = null;
+                try {
+                    List<Address> addresses = mGeocoder.getFromLocation(marker.getPosition().latitude, marker.getPosition().longitude ,1);
+                    address= addresses.get(0).getAddressLine(0);
+                } catch (IOException e) { e.printStackTrace(); }
                 try {
                     marker.setPosition(marker.getPosition());
                     marker.setSnippet(mGeocoder.getFromLocation(marker.getPosition().latitude, marker.getPosition().longitude, 1).get(0).getAddressLine(0));
                 } catch (IOException e) { e.printStackTrace(); }
-                if (mEndLoc != null) {
+                if (address != null && marker.getTitle().equals(getString(R.string.end_address_title))) {
                     mEndLoc = new LatLng(marker.getPosition().latitude, marker.getPosition().longitude);
+                    ((TextView) findViewById(R.id.txt_end_address)).setText(String.format(getString(R.string.route_end_prefix), address));
                 } else {
                     mStartLoc = new LatLng(marker.getPosition().latitude, marker.getPosition().longitude);
+                    ((TextView) findViewById(R.id.txt_start_address)).setText(String.format(getString(R.string.route_start_prefix), address));
                 }
                 marker.showInfoWindow();
             }
@@ -164,7 +171,7 @@ public class ActivityCreateRoute extends AppCompatActivity implements
             if (mStartLoc == null) {
                 Marker marker = gMap.addMarker(new MarkerOptions()
                         .position(latLng)
-                        .title("Start")
+                        .title(getString(R.string.start_address_title))
                         .snippet(address)
                         .flat(true)
                         .draggable(true));
@@ -176,7 +183,7 @@ public class ActivityCreateRoute extends AppCompatActivity implements
             } else if (mEndLoc == null) {
                 Marker marker = gMap.addMarker(new MarkerOptions()
                         .position(latLng)
-                        .title("End")
+                        .title(getString(R.string.end_address_title))
                         .snippet(address)
                         .flat(true)
                         .draggable(true));
