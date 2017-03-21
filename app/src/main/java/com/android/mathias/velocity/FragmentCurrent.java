5c8d2a782f81a8575caf1bb11d536240379350dd
Mathias Wöß <mat.woess@gmail.com>
2017-03-21 19:10:00 +0100

    replaced stop-fab by Button, removed start/pause/stop-function args, made buttons/fab member vars

--- app/src/main/java/com/android/mathias/velocity/FragmentCurrent.java
+++ app/src/main/java/com/android/mathias/velocity/FragmentCurrent.java
@@ -21,6 +21,7 @@ import android.view.ViewGroup;
 import android.view.animation.LinearInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ListPopupWindow;
 import android.widget.ProgressBar;
@@ -34,58 +35,61 @@ import java.util.List;
 public class FragmentCurrent extends android.support.v4.app.Fragment {
 
     private static int NOTIFICATION_ID = 1;
-    NotificationManager mNotificationManager;
     Chronometer mChronometer;
     ChronometerState mChronometerState;
     long mLastStopTime;
     Route mCurrentWalkRoute;
     ObjectAnimator mAnimator;
+    NotificationManager mNotificationManager;
+
+    FloatingActionButton mFab;
+    Button rBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View currentView = inflater.inflate(R.layout.fragment_current, container, false);
+        final View view = inflater.inflate(R.layout.fragment_current, container, false);
         setHasOptionsMenu(true);
-        mChronometer = (Chronometer) currentView.findViewById(R.id.stopwatch);
-        final FloatingActionButton fabPlayPause = (FloatingActionButton) currentView.findViewById(R.id.fab_current_play_pause);
-        final FloatingActionButton fabStop = (FloatingActionButton) currentView.findViewById(R.id.fab_current_stop);
-        fabPlayPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handlePlayPauseFabEvent(currentView, fabPlayPause);
-            }
-        });
-        fabStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                stopWalk(currentView, fabStop);
-            }
-        });
-        mChronometerState = ChronometerState.STOPPED;
-        ProgressBar progressBar = (ProgressBar) currentView.findViewById(R.id.progressBar);
-        mAnimator = ObjectAnimator.ofInt(progressBar, "progress", 0, 600);
-        mAnimator.setDuration(120000);
-        mAnimator.setInterpolator(new LinearInterpolator());
-        mAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        mNotificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        mChronometer = (Chronometer) view.findViewById(R.id.stopwatch);
         mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
                 buildNotification();
             }
         });
-        return currentView;
+        mChronometerState = ChronometerState.STOPPED;
+        mFab = (FloatingActionButton) view.findViewById(R.id.fab_current_play_pause);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleStopwatch();
+            }
+        });
+        rBtn = (Button) view.findViewById(R.id.fab_current_stop);
+        rBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopWalk();
+            }
+        });
+        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        mAnimator = ObjectAnimator.ofInt(progressBar, "progress", 0, 600);
+        mAnimator.setDuration(120000);
+        mAnimator.setInterpolator(new LinearInterpolator());
+        mAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        mNotificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        return view;
     }
 
-    private void handlePlayPauseFabEvent(final View currentView, final FloatingActionButton fab) {
+    private void toggleStopwatch() {
         switch (mChronometerState) {
-            case STOPPED: startWalk(currentView, fab); break;
-            case RUNNING: pauseStopwatch(fab); break;
-            case PAUSED:  resumeStopwatch(fab); break;
+            case STOPPED: startWalk(); break;
+            case RUNNING: pauseStopwatch(); break;
+            case PAUSED:  resumeStopwatch(); break;
             default: break;
         }
     }
 
-    private void startWalk(final View currentView, final FloatingActionButton fab) {
+    private void startWalk() {
         final List<String> routeNames = new ArrayList<>();
         for (Route r : DBManager.getRoutes(getContext(), null)) { routeNames.add(r.getName()); }
         String defaultRouteName = PreferenceManager.getDefaultSharedPreferences(getContext()).getString("default_route", "None");
@@ -97,8 +101,8 @@ public class FragmentCurrent extends android.support.v4.app.Fragment {
             adapter.add("None");
             final ListPopupWindow lpw = new ListPopupWindow(getContext());
             lpw.setAdapter(adapter);
-            lpw.setAnchorView(fab);
-            lpw.setContentWidth((int) (((View)fab.getParent()).getWidth()/2.5));
+            lpw.setAnchorView(mFab);
+            lpw.setContentWidth((int) (((View)mFab.getParent()).getWidth()/2.5));
             lpw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -107,60 +111,58 @@ public class FragmentCurrent extends android.support.v4.app.Fragment {
                     } else {
                         mCurrentWalkRoute = DBManager.getRoutes(getContext(), adapterView.getItemAtPosition(i).toString()).get(0);
                     }
-                    startStopwatch(currentView, fab);
+                    startStopwatch();
                     lpw.dismiss();
                 }
             });
             lpw.show();
         } else {
-            startStopwatch(currentView, fab);
+            startStopwatch();
         }
     }
 
-    private void startStopwatch(View currentView, FloatingActionButton fab) {
-        FloatingActionButton fabStop = (FloatingActionButton) currentView.findViewById(R.id.fab_current_stop);
+    private void startStopwatch() {
         mChronometer.setBase(SystemClock.elapsedRealtime());
         mChronometer.start();
-        fab.setImageResource(android.R.drawable.ic_media_pause);
+        mFab.setImageResource(android.R.drawable.ic_media_pause);
         mChronometerState = ChronometerState.RUNNING;
-        fabStop.setClickable(true);
-        fabStop.setVisibility(View.VISIBLE);
-        ((TextView)currentView.findViewById(R.id.txt_current_route)).setText(mCurrentWalkRoute.getName());
+        rBtn.setClickable(true);
+        rBtn.setVisibility(View.VISIBLE);
+        ((TextView)getActivity().findViewById(R.id.txt_current_route)).setText(mCurrentWalkRoute.getName());
         mAnimator.start();
         buildNotification();
     }
 
-    private void pauseStopwatch(FloatingActionButton fab) {
+    private void pauseStopwatch() {
         mChronometer.stop();
         mLastStopTime = SystemClock.elapsedRealtime();
-        fab.setImageResource(android.R.drawable.ic_media_play);
+        mFab.setImageResource(android.R.drawable.ic_media_play);
         mChronometerState = ChronometerState.PAUSED;
         mAnimator.pause();
         buildNotification();
     }
 
-    private void resumeStopwatch(FloatingActionButton fab) {
+    private void resumeStopwatch() {
         mChronometer.setBase(mChronometer.getBase() + (SystemClock.elapsedRealtime() - mLastStopTime));
         mChronometer.start();
-        fab.setImageResource(android.R.drawable.ic_media_pause);
+        mFab.setImageResource(android.R.drawable.ic_media_pause);
         mChronometerState = ChronometerState.RUNNING;
         mAnimator.resume();
         buildNotification();
     }
 
-    private void stopWalk(View currentView, FloatingActionButton fab) {
-        FloatingActionButton fabPlayPause = (FloatingActionButton) currentView.findViewById(R.id.fab_current_play_pause);
+    private void stopWalk() {
         mChronometer.stop();
         long walkTime = SystemClock.elapsedRealtime() - mChronometer.getBase();
         Walk walk = new Walk(walkTime, new Date(), mCurrentWalkRoute);
         DBManager.saveWalk(getContext(), walk);
         mLastStopTime = 0;
-        fabPlayPause.setImageResource(android.R.drawable.ic_media_play);
+        mFab.setImageResource(android.R.drawable.ic_media_play);
         mChronometerState = ChronometerState.STOPPED;
-        fab.setClickable(false);
-        fab.setVisibility(View.INVISIBLE);
+        rBtn.setClickable(false);
+        rBtn.setVisibility(View.INVISIBLE);
         mCurrentWalkRoute = null;
-        ((TextView)currentView.findViewById(R.id.txt_current_route)).setText("");
+        ((TextView)getActivity().findViewById(R.id.txt_current_route)).setText("");
         mAnimator.cancel();
         mNotificationManager.cancel(NOTIFICATION_ID);
     }
