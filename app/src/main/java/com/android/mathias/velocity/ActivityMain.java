6164dc7fae9087e078b6f2b2ebf79e3ac47c29f7
Mathias Wöß <mat.woess@gmail.com>
2017-05-29 22:22:06 +0200

    raised version to v0.5r, replaced nav drawer with bottom navigation bar, deleted some unneeded lines of code and files

--- app/src/main/java/com/android/mathias/velocity/ActivityMain.java
+++ app/src/main/java/com/android/mathias/velocity/ActivityMain.java
@@ -1,20 +1,16 @@
 package com.android.mathias.velocity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class ActivityMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ActivityMain extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +19,14 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.layout_main);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        NavigationView navigationView = findViewById(R.id.navigation);
-        navigationView.setNavigationItemSelectedListener(this);
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
         showFragment(FragmentCurrent.class);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.layout_main);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            this.moveTaskToBack(true);
-        }
+        this.moveTaskToBack(true);
     }
 
     @Override
@@ -55,9 +42,6 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
             case R.id.nav_routes:
                 fragment = FragmentRoutes.class;
                 break;
-            case R.id.nav_settings:
-                startActivity(new Intent(this, ActivitySettings.class));
-                break;
             default: break;
         }
         if (fragment != null && !item.isChecked()) {
@@ -65,8 +49,6 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
             item.setChecked(true);
             setTitle(item.getTitle());
         }
-        DrawerLayout drawer = findViewById(R.id.layout_main);
-        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
